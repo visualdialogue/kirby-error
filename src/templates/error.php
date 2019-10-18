7324242bfb170bc65dc7b1478187910dc57ce107
Luke Hatfield <luke@lukehatfield.com>
2019-10-18 10:22:54 -0400

    gulp working again

--- src/templates/error.php
+++ src/templates/error.php
@@ -1,16 +1,11 @@
 
-<div class="footer-social-media">
-  <div class="footer-social-media-title">Follow</div>
-  <div class="footer-social-media-list">
-    <!-- no li b/c a tag needs to be wrapper so can use susy grid measurements of parent for padding to match negative margins--><a href="<?= option('visualdialogue.socialicons.facebook') ?>" target="_blank" class="footer-social-media-link">
-      <svg viewBox="0 0 16.36 16.4" xml:space="preserve" class="footer-social-media-icon">
-        <use xlink:href="#facebook"></use>
-      </svg></a><a href="<?= option('visualdialogue.socialicons.instagram') ?>" target="_blank" class="footer-social-media-link">
-      <svg viewBox="0 0 345.4 345.4" xml:space="preserve" class="footer-social-media-icon">
-        <use xlink:href="#instagram"></use>
-      </svg></a><a href="<?= option('visualdialogue.socialicons.twitter') ?>" target="_blank" class="footer-social-media-link">
-      <svg viewBox="0 0 18.9 15.33" xml:space="preserve" class="footer-social-media-icon">
-        <use xlink:href="#twitter"></use>
-      </svg></a>
+<?php snippet('header'); ?>
+<main>
+  <div class="error-page-copy">
+    <h1 class="error-title">
+      <?php echo $page->title()->html(); ?></h1>
+    <div class="error-text">
+      <?php echo $page->text()->kirbytext(); ?></div>
   </div>
-</div>
+</main>
+<?php snippet('footer'); ?>
